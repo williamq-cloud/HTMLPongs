dc046ba2034040edfeacdc97dc299b15f610d334
Entity404 <williamqin2011@gmail.com>
2025-06-23 15:58:26 -0700

    Added 1player and allbots mode
    Uses HTML, CSS, and JS

---
+++ HTMLPong1Player/app.js
@@ -0,0 +1,206 @@
+var canvas;
+var canvasContext;
+
+var ballX = 400;
+var ballY = 600;
+var ballSize = 10;
+var ballSpeedX = 10;
+var ballSpeedY = 10;
+var deltaY = 0;
+var amtSteps;
+const maxBallSpeed = 40;
+
+const PADDLE_HEIGHT = 100;
+const PADDLE_WIDTH = 10;
+var paddle1Y;
+var paddle2Y;
+
+var botSpeed = 8;
+
+var player1Score = 0;
+var player2Score = 0;
+const winScore = 5;
+var showWinScreen = false;
+
+function handleMouseClick(event) {
+    if (showWinScreen) {
+        player1Score = 0;
+        player2Score = 0;
+        showWinScreen = false;
+    }
+}
+
+window.onload = () => {
+    console.log("Game loaded!"); // Game has loaded
+    canvas = document.getElementById('gameCanvas');
+    canvasContext = canvas.getContext('2d');
+    
+    paddle1Y = (canvas.height / 2) - 50;
+    paddle2Y = (canvas.height / 2) - 50;
+
+    ballX = canvas.width / 2;
+    ballY = canvas.height / 2;
+
+    var FPS = 30;
+    setInterval(() => {
+        move();
+        draw();
+    }, 1000/FPS);
+
+    canvas.addEventListener('mousemove', (event) => {
+        var mousePos = calcMousePos(event);
+        paddle1Y = mousePos.y - (PADDLE_HEIGHT / 2);
+    });
+    canvas.addEventListener('mousedown', handleMouseClick);
+};
+
+function calcMousePos(event) {
+    var rect = canvas.getBoundingClientRect();
+    var root = document.documentElement;
+    var mouseX = event.clientX - rect.left - root.scrollLeft;
+    var mouseY = event.clientY - rect.top - root.scrollTop;
+
+    return {
+        x: mouseX,
+        y: mouseY
+    };
+}
+
+function ballReset() {
+    if (player1Score >= winScore || player2Score >= winScore) {
+        showWinScreen = true;
+    }
+    ballSpeedX = 10;
+    ballSpeedX = -ballSpeedX;
+    ballSpeedY = 10;
+    ballSpeedY = -ballSpeedY;
+    ballX = canvas.width / 2;
+    ballY = canvas.height / 2;
+}
+
+function botMove() {
+    var paddle2YCenter = paddle2Y + (PADDLE_HEIGHT / 2);
+
+    if (paddle2YCenter < (ballY - 35)) {
+        paddle2Y += botSpeed;
+    } else if (paddle2YCenter > (ballY + 35)) {
+        paddle2Y -= botSpeed;
+    }
+}
+
+function speedUpBall() {
+    ballSpeedX *= 1.1;
+    ballSpeedY *= 1.1;
+
+    ballSpeedX = Math.max(Math.min(ballSpeedX, maxBallSpeed), -maxBallSpeed);
+    ballSpeedY = Math.max(Math.min(ballSpeedY, maxBallSpeed), -maxBallSpeed);
+}
+
+function move() {
+    if (showWinScreen) {
+        return;
+    }
+
+    botMove();
+
+    amtSteps = Math.ceil(Math.max(Math.abs(ballSpeedX), Math.abs(ballSpeedY)))
+    for (let step = 0; step < amtSteps; step += 1) {
+        ballX += ballSpeedX / amtSteps; 
+        ballY += ballSpeedY / amtSteps;
+
+        if (ballX < 0) {
+            if (ballY > paddle1Y && ballY < (paddle1Y + PADDLE_HEIGHT)) {
+                speedUpBall();
+                ballSpeedX = -ballSpeedX;
+
+                deltaY = ballY - (paddle1Y + (PADDLE_HEIGHT / 2));
+                ballSpeedY = deltaY * 0.35;
+            } else {
+                player2Score += 1;
+                ballReset();
+            }
+        }
+
+        if (ballX > canvas.width) {
+            if (ballY > paddle2Y && ballY < (paddle2Y + PADDLE_HEIGHT)) {
+                speedUpBall();
+                ballSpeedX = -ballSpeedX;
+
+                deltaY = ballY - (paddle2Y + (PADDLE_HEIGHT / 2));
+                ballSpeedY = deltaY * 0.35;
+            } else {
+                player1Score += 1;
+                ballReset();
+            }
+        }
+
+        if (ballY > canvas.height) {
+            ballSpeedY = -ballSpeedY;
+        }
+        if (ballY < 0) {
+            ballSpeedY = -ballSpeedY;
+        }
+    }
+}
+
+function drawNet() {
+    for (var i = 0; i < canvas.height; i += 20) {
+        drawRect(((canvas.width / 2) - 1), i, 2, 10, 'white');
+    }
+}
+
+function draw() {
+    canvasContext.fillStyle = 'black';
+    canvasContext.fillRect(0, 0, canvas.width, canvas.height);
+
+    if (showWinScreen) {
+        if (player1Score >= winScore) {
+            canvasContext.fillStyle = 'white';
+            canvasContext.font = "60px monospace";
+            canvasContext.textAlign = "center";
+            canvasContext.fillText("You Won!", (canvas.width / 2), ((canvas.height / 2) - 50))
+        } else {
+            canvasContext.fillStyle = 'white';
+            canvasContext.font = "60px monospace";
+            canvasContext.textAlign = "center";
+            canvasContext.fillText("The Bot Won!", (canvas.width / 2), ((canvas.height / 2) - 50))
+        }
+
+        canvasContext.fillStyle = 'white';
+        canvasContext.font = "45px monospace";
+        canvasContext.textAlign = "center";
+        canvasContext.fillText("Click to play again", (canvas.width / 2), ((canvas.height / 2) + 50))
+        return;
+    }
+
+    drawNet();
+
+    drawRect(0, paddle1Y, PADDLE_WIDTH, PADDLE_HEIGHT, 'white');
+    drawRect((canvas.width - PADDLE_WIDTH), paddle2Y, PADDLE_WIDTH, PADDLE_HEIGHT, 'white');
+    
+    drawCircle(ballX, ballY, ballSize, 'white');
+
+    canvasContext.font = "75px monospace";
+    canvasContext.textAlign = "center";
+
+    canvasContext.fillText(player1Score, (canvas.width / 4), 100);
+    canvasContext.fillText(player2Score, ((canvas.width * 3) / 4), 100);
+
+    canvasContext.font = "25px monospace";
+    canvasContext.textAlign = "center";
+
+    canvasContext.fillText("You", (canvas.width / 4), 130);
+    canvasContext.fillText("Bot", ((canvas.width * 3) / 4), 130);
+}
+
+function drawRect(leftX, topY, width, height, drawColor) {
+    canvasContext.fillStyle = drawColor;
+    canvasContext.fillRect(leftX, topY, width, height);
+}
+
+function drawCircle(x, y, radius, color) {
+    canvasContext.fillStyle = color;
+    canvasContext.beginPath();
+    canvasContext.arc(x, y, radius, 0, Math.PI * 2, true);
+    canvasContext.fill();
+}
